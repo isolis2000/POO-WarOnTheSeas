d128f1f12854bdb3114231154a69a6fa5f68f971
Ivan <isolis@solavcr.com>
2022-05-29 19:45:04 -0600

    [ADD] Thunders under the sea

--- src/main/java/gamelogic/fighters/ThundersUnderTheSea.java
+++ src/main/java/gamelogic/fighters/ThundersUnderTheSea.java
@@ -4,9 +4,12 @@
  */
 package gamelogic.fighters;
 
+import client.gui.Cell;
 import gamelogic.Fighter;
 import gamelogic.Player;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
 import server.ThreadServer;
 
 /**
@@ -18,10 +21,94 @@ public class ThundersUnderTheSea extends Fighter {
     public ThundersUnderTheSea(String name, String image, int percentage, int power, int resistance, int sanity, Color color, Player playerExecuting) {
         super(name, image, percentage, power, resistance, sanity, color, playerExecuting);
     }
-
     @Override
     protected boolean specialAttack(String[] args, ThreadServer target) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        boolean result = 
+        switch (args[3].toUpperCase()) {
+            case "THUNDER RAIN" -> thunderRain(target);
+            case "POSEIDON THUNDERS" -> poseidonThunders(target);
+            case "EEL ATTACK" -> eelAttack(target);
+            default ->false;
+        };
+        if (!result)
+            System.out.println("specialAttackBombsucks");
+        return result;
+    }
+    /*
+    Thunder rain: se creará una lluvia de 100
+    rayos, cada rayo daña el 10-20% de la casilla
+    donde cae.
+    */
+    private boolean thunderRain(ThreadServer target) {
+        Random random = new Random();
+        try {
+            String forRecord = "Jugador " + this.playerExecuting.getPlayerName() 
+                    + " ataco esta casilla con el ataque Thunder Rain." 
+                    + " La casilla tomo ";
+            ArrayList<Cell> cellsHit = target.getPlayer().getRandomCells(100);
+            for (Cell cell : cellsHit) {
+                int damageTaken = random.nextInt(10,21);
+                forRecord += Integer.toString(damageTaken) + "% de dano.";
+                cell.takeDamage(damageTaken, forRecord);
+            }
+            return true;
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+    /*
+    Poseidon thunders: crea entre 5 y 10 rayos,
+    que al hacer contacto con una casilla genera
+    una onda de destrucción de entre 2 y 10
+    casillas de radio.
+    */
+    private boolean poseidonThunders(ThreadServer target) {
+        Random random = new Random();
+        int ammountOfThunders = random.nextInt(5, 11);
+        try {
+            String forRecord = "Jugador " + this.playerExecuting.getPlayerName() 
+                    + " destruyo esta casilla con el ataque Poseidon Thunders";
+            ArrayList<Cell> initialHits = target.getPlayer().getRandomCells(ammountOfThunders);
+            for (Cell initialCell : initialHits) {
+                int radius = random.nextInt(2, 11);
+                ArrayList<Cell> colateralCells = target.getPlayer().getCellsInRadius(initialCell.getPlacement(), radius);
+                for (Cell cell : colateralCells)
+                    cell.setHp(0, forRecord);
+            }
+            return true;
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+    /*
+    Eel atack: se creará un ataque de anguilas,
+    entre 25 y 100 anguilas. Cada anguila dará
+    entre 1 y 10 descargas eléctricas a la casilla
+    donde ataca. Cada descarga daña un 10% de
+    la casilla.
+    */
+    private boolean eelAttack(ThreadServer target) {
+        Random random = new Random();
+        int numOfEels = random.nextInt(25, 101);
+        try {
+            String forRecord = "Jugador " + this.playerExecuting.getPlayerName() 
+                    + " ataco esta casilla con el ataque Eel Attack." 
+                    + " La casilla recibio ";
+            ArrayList<Cell> cellsToAttack = target.getPlayer().getRandomCells(numOfEels);
+            for (Cell cell : cellsToAttack) {
+                int numOfShocks = random.nextInt(10) + 1;
+                int damage = numOfShocks * 10;
+                forRecord += Integer.toString(numOfShocks) + " descargas y tomo " 
+                        + Integer.toString(damage) + "% de dano.";
+                cell.takeDamage(damage, forRecord);
+            }
+            return true;
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+            return false;
+        }
     }
     
     
